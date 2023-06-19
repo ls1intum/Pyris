e41ccc857b931ccc9c45950b84f89a81abda65ee
Khoa Nguyen <dakennguyen@gmail.com>
2023-06-19 12:18:19 +0200

    Use application.test.yml for test env

--- app/config.py
+++ app/config.py
@@ -1,3 +1,4 @@
+import os
 from pydantic import BaseModel
 from pyaml_env import parse_config
 
@@ -9,5 +10,14 @@ class Settings(BaseModel):
 
     pyris: PyrisSettings
 
+    @classmethod
+    def get_settings(cls):
+        if "RUN_ENV" in os.environ and os.environ["RUN_ENV"] == "test":
+            file_path = "application.test.yml"
+        else:
+            file_path = "application.yml"
 
-settings = Settings.parse_obj(parse_config("application.yml"))
+        return Settings.parse_obj(parse_config(file_path))
+
+
+settings = Settings.get_settings()
