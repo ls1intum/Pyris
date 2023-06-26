eaf880843afe30127d8f6d2eaa05e010024f43a6
Timor Morrien <timor.morrien@tum.de>
2023-06-26 15:02:42 +0200

    Add check

--- app/services/guidance_wrapper.py
+++ app/services/guidance_wrapper.py
@@ -27,8 +27,9 @@ class GuidanceWrapper:
             ValueError: if handlebars do not generate 'response'
         """
 
-        self.parameters["studentRepository"]["BinSea_java"] = \
-            self.parameters["studentRepository"]["src/gad/binarysearch/BinSea.java"]
+        # Check if studentRepository is in parameters
+        if "studentRepository" in self.parameters and "src/gad/binarysearch/BinSea.java" in self.parameters["studentRepository"]:
+            self.parameters["studentRepository"]["BinSea_java"] = self.parameters["studentRepository"]["src/gad/binarysearch/BinSea.java"]
 
         template = guidance(self.handlebars)
         result = template(
