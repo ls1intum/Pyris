0cb68d9e8fdfc976d092843e5e59bc7e4c77bdd9
Khoa Nguyen <dakennguyen@gmail.com>
2023-07-06 19:33:23 +0200

    Add in memory cache store

--- app/services/cache.py
+++ app/services/cache.py
@@ -1,4 +1,82 @@
-from app.config import settings
+from abc import ABC, abstractmethod
+from datetime import datetime, timedelta
 
-# TODO
-cache_store = None
+
+class CacheStoreInterface(ABC):
+    @abstractmethod
+    def get(self, key):
+        pass
+
+    @abstractmethod
+    def set(self, key, value, ex=None):
+        pass
+
+    @abstractmethod
+    def expire(self, key, ex):
+        pass
+
+    @abstractmethod
+    def incr(self, key):
+        """Increase the integer value of a key by 1.
+        If the key does not exist, it is set to 0
+        before performing the operation.
+        """
+        pass
+
+    @abstractmethod
+    def flushdb(self):
+        pass
+
+    @abstractmethod
+    def delete(self, key):
+        pass
+
+
+class InMemoryCacheStore(CacheStoreInterface):
+    def __init__(self):
+        self._cache = {}
+
+    def get(self, key):
+        current_time = datetime.now()
+        ttl = self._cache.get(f"{key}:ttl")
+        if ttl and current_time > ttl:
+            del self._cache[key]
+            del self._cache[f"{key}:ttl"]
+            return None
+
+        return self._cache.get(key)
+
+    def set(self, key, value, ex=None):
+        self._cache[key] = value
+        self.expire(key, ex)
+
+    def expire(self, key, ex):
+        if ex is None:
+            ttl = None
+        else:
+            current_time = datetime.now()
+            ttl = current_time + timedelta(seconds=ex)
+
+        self._cache[f"{key}:ttl"] = ttl
+
+    def incr(self, key):
+        value = self.get(key)
+        if value is None:
+            self._cache[key] = 1
+        elif isinstance(value, int):
+            self._cache[key] += 1
+        else:
+            raise TypeError("value is not an integer")
+
+        return self._cache[key]
+
+    def flushdb(self):
+        self._cache = {}
+
+    def delete(self, key):
+        if key in self._cache:
+            del self._cache[key]
+            del self._cache[f"{key}:ttl"]
+
+
+cache_store = InMemoryCacheStore()
---
+++ tests/services/cache_test.py
@@ -0,0 +1,105 @@
+import pytest
+from datetime import datetime
+from freezegun import freeze_time
+
+
+class TestInMemoryCacheStore:
+    def test_set_and_get(self, test_cache_store):
+        test_cache_store.set("test_key", "test_value")
+        assert test_cache_store.get("test_key") == "test_value"
+        assert test_cache_store.get("test_key:ttl") is None
+
+    @freeze_time("2023-06-16 03:21:34 +02:00")
+    def test_set_and_get_an_existing_key(self, test_cache_store):
+        test_cache_store.set("test_key", "test_value", ex=30)
+        assert test_cache_store.get("test_key") == "test_value"
+        assert test_cache_store.get("test_key:ttl") == datetime(
+            year=2023, month=6, day=16, hour=1, minute=22, second=4
+        )
+
+        test_cache_store.set("test_key", "test_value2")
+        assert test_cache_store.get("test_key") == "test_value2"
+        assert test_cache_store.get("test_key:ttl") is None
+
+    def test_set_and_get_before_ttl(self, test_cache_store):
+        initial_datetime = datetime(
+            year=1, month=1, day=1, hour=15, minute=2, second=3
+        )
+        other_datetime = datetime(
+            year=1, month=1, day=1, hour=15, minute=2, second=34
+        )
+
+        with freeze_time(initial_datetime) as frozen_datetime:
+            test_cache_store.set("test_key", "test_value", ex=40)
+            assert test_cache_store.get("test_key") == "test_value"
+            assert test_cache_store.get("test_key:ttl") == datetime(
+                year=1, month=1, day=1, hour=15, minute=2, second=43
+            )
+
+            frozen_datetime.move_to(other_datetime)
+
+            assert test_cache_store.get("test_key") == "test_value"
+            assert test_cache_store.get("test_key:ttl") == datetime(
+                year=1, month=1, day=1, hour=15, minute=2, second=43
+            )
+
+    def test_set_and_get_after_ttl(self, test_cache_store):
+        initial_datetime = datetime(
+            year=1, month=1, day=1, hour=15, minute=2, second=3
+        )
+        other_datetime = datetime(
+            year=1, month=1, day=1, hour=15, minute=2, second=34
+        )
+
+        with freeze_time(initial_datetime) as frozen_datetime:
+            test_cache_store.set("test_key", "test_value", ex=30)
+            assert test_cache_store.get("test_key") == "test_value"
+            assert test_cache_store.get("test_key:ttl") == datetime(
+                year=1, month=1, day=1, hour=15, minute=2, second=33
+            )
+
+            frozen_datetime.move_to(other_datetime)
+
+            assert test_cache_store.get("test_key") is None
+            assert test_cache_store.get("test_key:ttl") is None
+
+    @freeze_time("2023-06-16 03:21:34 +02:00")
+    def test_expire(self, test_cache_store):
+        test_cache_store.expire("test_key", 30)
+        assert test_cache_store.get("test_key:ttl") == datetime(
+            year=2023, month=6, day=16, hour=1, minute=22, second=4
+        )
+
+    def test_incr_a_non_existing_key(self, test_cache_store):
+        test_cache_store.incr("test_key")
+        assert test_cache_store.get("test_key") == 1
+
+    def test_incr_a_existing_key(self, test_cache_store):
+        test_cache_store.set("test_key", 1)
+        test_cache_store.incr("test_key")
+        assert test_cache_store.get("test_key") == 2
+
+    def test_incr_a_existing_key_not_a_number(self, test_cache_store):
+        test_cache_store.set("test_key", "not a number")
+
+        with pytest.raises(TypeError, match="value is not an integer"):
+            test_cache_store.incr("test_key")
+
+    def test_flushdb(self, test_cache_store):
+        test_cache_store.set("test_key", "test_value")
+        test_cache_store.flushdb()
+
+        assert test_cache_store._cache == {}
+
+    def test_delete_a_non_existing_key(self, test_cache_store):
+        test_cache_store.delete("test_key")
+
+        assert test_cache_store.get("test_key") is None
+        assert test_cache_store.get("test_key:ttl") is None
+
+    def test_delete_a_existing_key(self, test_cache_store):
+        test_cache_store.set("test_key", "test_value", ex=1000)
+        test_cache_store.delete("test_key")
+
+        assert test_cache_store.get("test_key") is None
+        assert test_cache_store.get("test_key:ttl") is None
