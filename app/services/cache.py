ded2ff71e7e54d58cb4a391c50a47999fd1c3d2f
Khoa Nguyen <dakennguyen@gmail.com>
2023-07-10 17:14:08 +0200

    Add docstring for cache interface

--- app/services/cache.py
+++ app/services/cache.py
@@ -4,20 +4,31 @@ from datetime import datetime, timedelta
 
 class CacheStoreInterface(ABC):
     @abstractmethod
-    def get(self, key):
+    def get(self, name):
+        """
+        Return the value at key ``name``, or None if the key doesn't exist
+        """
         pass
 
     @abstractmethod
-    def set(self, key, value, ex=None):
+    def set(self, name, value, ex=None):
+        """
+        Set the value at key ``name`` to ``value``
+
+        ``ex`` sets an expire flag on key ``name`` for ``ex`` seconds.
+        """
         pass
 
     @abstractmethod
-    def expire(self, key, ex):
+    def expire(self, name, ex):
+        """
+        Set an expire flag on key ``name`` for ``ex`` seconds
+        """
         pass
 
     @abstractmethod
-    def incr(self, key):
-        """Increase the integer value of a key by 1.
+    def incr(self, name):
+        """Increase the integer value of a key ``name`` by 1.
         If the key does not exist, it is set to 0
         before performing the operation.
         """
@@ -25,10 +36,16 @@ class CacheStoreInterface(ABC):
 
     @abstractmethod
     def flushdb(self):
+        """
+        Delete all keys in the current database
+        """
         pass
 
     @abstractmethod
-    def delete(self, key):
+    def delete(self, name):
+        """
+        Delete the key specified by ``name``
+        """
         pass
 
 
@@ -36,47 +53,47 @@ class InMemoryCacheStore(CacheStoreInterface):
     def __init__(self):
         self._cache = {}
 
-    def get(self, key):
+    def get(self, name):
         current_time = datetime.now()
-        ttl = self._cache.get(f"{key}:ttl")
+        ttl = self._cache.get(f"{name}:ttl")
         if ttl and current_time > ttl:
-            del self._cache[key]
-            del self._cache[f"{key}:ttl"]
+            del self._cache[name]
+            del self._cache[f"{name}:ttl"]
             return None
 
-        return self._cache.get(key)
+        return self._cache.get(name)
 
-    def set(self, key, value, ex=None):
-        self._cache[key] = value
-        self.expire(key, ex)
+    def set(self, name, value, ex=None):
+        self._cache[name] = value
+        self.expire(name, ex)
 
-    def expire(self, key, ex):
+    def expire(self, name, ex):
         if ex is None:
             ttl = None
         else:
             current_time = datetime.now()
             ttl = current_time + timedelta(seconds=ex)
 
-        self._cache[f"{key}:ttl"] = ttl
+        self._cache[f"{name}:ttl"] = ttl
 
-    def incr(self, key):
-        value = self.get(key)
+    def incr(self, name):
+        value = self.get(name)
         if value is None:
-            self._cache[key] = 1
+            self._cache[name] = 1
         elif isinstance(value, int):
-            self._cache[key] += 1
+            self._cache[name] += 1
         else:
             raise TypeError("value is not an integer")
 
-        return self._cache[key]
+        return self._cache[name]
 
     def flushdb(self):
         self._cache = {}
 
-    def delete(self, key):
-        if key in self._cache:
-            del self._cache[key]
-            del self._cache[f"{key}:ttl"]
+    def delete(self, name):
+        if name in self._cache:
+            del self._cache[name]
+            del self._cache[f"{name}:ttl"]
 
 
 cache_store = InMemoryCacheStore()
