a4750dd993dec1ef3583cb805b28a3f565acd3d2
Frederic Schönberger <24734883+fschoenberger@users.noreply.github.com>
2023-07-11 11:48:36 +0200

    Reformat code.

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from prometheus_fastapi_instrumentator import Instrumentator
-from prometheus_client import  Gauge
+from prometheus_client import Gauge
 import psutil
 import os
 
