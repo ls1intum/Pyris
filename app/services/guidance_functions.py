b2abb60cc4b0bf767b19cc6c4fc8cd5c6ee92920
Timor Morrien <timor.morrien@tum.de>
2023-07-19 13:13:01 +0200

    Add truncate function

---
+++ app/services/guidance_functions.py
@@ -0,0 +1,3 @@
+def truncate(history: list[any], max_length: int):
+    return history[-max_length:]
+
--- app/services/guidance_wrapper.py
+++ app/services/guidance_wrapper.py
@@ -2,6 +2,7 @@ import guidance
 
 from app.config import LLMModelConfig
 from app.models.dtos import Content, ContentType
+from app.services.guidance_functions import truncate
 
 
 class GuidanceWrapper:
@@ -30,6 +31,7 @@ class GuidanceWrapper:
         template = guidance(self.handlebars)
         result = template(
             llm=self._get_llm(),
+            truncate=truncate,
             **self.parameters,
         )
 
