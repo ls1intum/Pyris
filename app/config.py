8298e7810af12c8b911cabb217ccc5b0174ed578
Timor Morrien <timor.morrien@tum.de>
2024-01-08 14:05:47 +0100

    Add if check to problematic place

--- app/config.py
+++ app/config.py
@@ -16,7 +16,10 @@ def new_add_text_to_chat_mode(chat_mode):
     else:
         print("new_add_text_to_chat_mode", chat_mode.items())
         for c in chat_mode["choices"]:
-            c["text"] = c["message"]["content"]
+            if "message" in c and "content" in c["message"]:
+                c["text"] = c["message"]["content"]
+            else:
+                c["text"] = ""
         return chat_mode
 
 
