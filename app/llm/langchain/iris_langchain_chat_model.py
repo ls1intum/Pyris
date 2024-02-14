16b72b016f83cf46595b47ac044b0ab8ceb0dc75
Timor Morrien <timor.morrien@tum.de>
2024-02-14 15:07:47 +0100

    Add all required Langchain LLM wrappers

--- app/llm/langchain/iris_langchain_chat_model.py
+++ app/llm/langchain/iris_langchain_chat_model.py
@@ -6,17 +6,30 @@ from langchain_core.language_models.chat_models import (
 )
 from langchain_core.messages import BaseMessage
 from langchain_core.outputs import ChatResult
+from langchain_core.outputs.chat_generation import ChatGeneration
 
-from domain import IrisMessage
+from domain import IrisMessage, IrisMessageRole
 from llm import RequestHandlerInterface, CompletionArguments
 
 
 def convert_iris_message_to_base_message(iris_message: IrisMessage) -> BaseMessage:
-    return BaseMessage(content=iris_message.text, role=iris_message.role)
+    role_map = {
+        IrisMessageRole.USER: "human",
+        IrisMessageRole.ASSISTANT: "ai",
+        IrisMessageRole.SYSTEM: "system",
+    }
+    return BaseMessage(content=iris_message.text, type=role_map[iris_message.role])
 
 
 def convert_base_message_to_iris_message(base_message: BaseMessage) -> IrisMessage:
-    return IrisMessage(text=base_message.content, role=base_message.role)
+    role_map = {
+        "human": IrisMessageRole.USER,
+        "ai": IrisMessageRole.ASSISTANT,
+        "system": IrisMessageRole.SYSTEM,
+    }
+    return IrisMessage(
+        text=base_message.content, role=IrisMessageRole(role_map[base_message.type])
+    )
 
 
 class IrisLangchainChatModel(BaseChatModel):
@@ -25,8 +38,7 @@ class IrisLangchainChatModel(BaseChatModel):
     request_handler: RequestHandlerInterface
 
     def __init__(self, request_handler: RequestHandlerInterface, **kwargs: Any) -> None:
-        super().__init__(**kwargs)
-        self.request_handler = request_handler
+        super().__init__(request_handler=request_handler, **kwargs)
 
     def _generate(
         self,
@@ -35,11 +47,13 @@ class IrisLangchainChatModel(BaseChatModel):
         run_manager: Optional[CallbackManagerForLLMRun] = None,
         **kwargs: Any
     ) -> ChatResult:
+        iris_messages = [convert_base_message_to_iris_message(m) for m in messages]
         iris_message = self.request_handler.chat_completion(
-            messages, CompletionArguments(stop=stop)
+            iris_messages, CompletionArguments(stop=stop)
         )
         base_message = convert_iris_message_to_base_message(iris_message)
-        return ChatResult(generations=[base_message])
+        chat_generation = ChatGeneration(message=base_message)
+        return ChatResult(generations=[chat_generation])
 
     @property
     def _llm_type(self) -> str:
