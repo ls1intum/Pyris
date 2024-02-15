b18b16e14198da5f6b75a4775806363037101983
Kaan Çaylı <y.k.cayli@gmail.com>
2024-02-15 01:10:38 +0100

    Add pipeline base class and a simple pipeline

---
+++ app/pipeline/__init__.py
@@ -0,0 +1,1 @@
+from pipeline.pipeline import SimplePipeline
---
+++ app/pipeline/pipeline.py
@@ -0,0 +1,37 @@
+from abc import ABCMeta, abstractmethod
+from operator import itemgetter
+
+from langchain_core.output_parsers import StrOutputParser
+from domain import IrisMessage, IrisMessageRole
+
+
+class BasePipeline(metaclass=ABCMeta):
+    """ Abstract class for all pipelines """
+    def __init__(self, name=None):
+        self.name = name
+
+    def __repr__(self):
+        return f'{self.__class__.__name__} {self.name if self.name is not None else id(self)}'
+
+    def __str__(self):
+        return f'{self.__class__.__name__} {self.name if self.name is not None else id(self)}'
+
+    @abstractmethod
+    def run(self, *args, **kwargs) -> IrisMessage:
+        """ Run the pipeline """
+        raise NotImplementedError
+
+
+class SimplePipeline(BasePipeline):
+    def __init__(self, llm, name=None):
+        super().__init__(name=name)
+        self.llm = llm
+        self.pipeline = {"query": itemgetter("query")} | llm | StrOutputParser()
+
+    def run(self, *args, query: IrisMessage, **kwargs) -> IrisMessage:
+        """ A simple pipeline that does not have any memory etc."""
+        if query is None:
+            raise ValueError("IrisMessage must not be None")
+        message = query.text
+        response = self.pipeline.invoke({"query": message})
+        return IrisMessage(role=IrisMessageRole.ASSISTANT, text=response)
