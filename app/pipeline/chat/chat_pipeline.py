d7355b45d0b4dadfba98972dbac78a31fae89900
Michael Dyer <michaelowendyer@gmail.com>
2024-02-19 14:55:44 +0100

    Generalize pipeline superclass Not all pipelines will return an IrisMessage

---
+++ app/pipeline/chat/chat_pipeline.py
@@ -0,0 +1,18 @@
+from abc import ABC, abstractmethod
+
+from domain import IrisMessage
+from pipeline import Pipeline
+
+
+class ProgrammingExerciseTutorChatPipeline(Pipeline, ABC):
+    """Abstract class for the programming exercise tutor chat pipeline implementations"""
+
+    def __call__(self, query: IrisMessage, **kwargs) -> IrisMessage:
+        return self._run(query)
+
+    @abstractmethod
+    def _run(self, query: IrisMessage) -> IrisMessage:
+        """
+        Runs the pipeline and returns the response message.
+        """
+        raise NotImplementedError
