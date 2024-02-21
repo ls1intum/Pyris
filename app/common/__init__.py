2ebdb960810a223fe4de86dc01330b4021077402
Timor Morrien <timor.morrien@tum.de>
2024-02-21 12:56:33 +0100

    Rename file

--- app/common/__init__.py
+++ app/common/__init__.py
@@ -1,5 +1,5 @@
 from common.singleton import Singleton
-from common.util import (
+from common.message_converters import (
     convert_iris_message_to_langchain_message,
     convert_langchain_message_to_iris_message,
 )
