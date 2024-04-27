6cf2f4ec9ac54cdd3823502cc14ce027d1c6e7d2
Yassine Souissi <74144843+yassinsws@users.noreply.github.com>
2024-04-27 17:24:58 +0200

    Update image_message_content_dto.py

--- app/domain/data/image_message_content_dto.py
+++ app/domain/data/image_message_content_dto.py
@@ -3,14 +3,5 @@ from typing import List, Optional
 
 
 class ImageMessageContentDTO(BaseModel):
-    base64: List[str] = Field(..., alias="base64")  # List of base64-encoded strings
-    prompt: Optional[str] = Field(default=None, alias="prompt")
-
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "prompt": "Example prompt",
-                "base64": ["base64EncodedString==", "anotherBase64EncodedString=="],
-            }
-        }
-        populate_by_name = True
+    base64: List[str] 
+    prompt: Optional[str] 
