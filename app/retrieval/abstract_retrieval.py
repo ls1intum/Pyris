ea32c7b113131d0531de401ae8640feda0f6f45f
Yassine Souissi <yassine.souissi@tum.de>
2024-05-05 15:19:21 +0200

    Erase content_service

---
+++ app/retrieval/abstract_retrieval.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from typing import List
+
+
+class AbstractRetrieval(ABC):
+    """
+    Abstract class for retrieving data from a database.
+    """
+
+    @abstractmethod
+    def retrieve(self, path: str, hybrid_factor: float, result_limit: int) -> List[str]:
+        """
+        Abstract method to retrieve data from the database.
+        """
+        pass
---
+++ app/retrieval/lecture_retrieval.py
@@ -0,0 +1,45 @@
+from abc import ABC
+from typing import List
+
+import weaviate
+import weaviate.classes as wvc
+
+from app.retrieval.abstract_retrieval import AbstractRetrieval
+from app.vector_database.lectureschema import init_lecture_schema, LectureSchema
+
+
+class LectureRetrieval(AbstractRetrieval, ABC):
+    """
+    Class for retrieving lecture data from the database.
+    """
+
+    def __init__(self, client: weaviate.WeaviateClient):
+        self.collection = init_lecture_schema(client)
+
+    def retrieve(
+        self,
+        user_message: str,
+        hybrid_factor: float,
+        result_limit: int,
+        lecture_id: int = None,
+        message_vector: [float] = None,
+    ) -> List[str]:
+        response = self.collection.query.hybrid(
+            query=user_message,
+            filters=(
+                wvc.query.Filter.by_property(LectureSchema.LECTURE_ID.value).equal(
+                    lecture_id
+                )
+                if lecture_id
+                else None
+            ),
+            alpha=hybrid_factor,
+            vector=message_vector,
+            return_properties=[
+                LectureSchema.PAGE_TEXT_CONTENT.value,
+                LectureSchema.PAGE_IMAGE_DESCRIPTION.value,
+                LectureSchema.COURSE_NAME.value,
+            ],
+            limit=result_limit,
+        )
+        return response
