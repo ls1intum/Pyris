8f6f573a72d43e0475740fc8213f03509fd8fae9
Yassine Souissi <74144843+yassinsws@users.noreply.github.com>
2024-05-08 16:29:23 +0200

    `Ingester & Retriever`: Add support for Weaviate (#64)

---
+++ app/retrieval/lecture_retrieval.py
@@ -0,0 +1,43 @@
+from abc import ABC
+from typing import List
+
+from weaviate import WeaviateClient
+from weaviate.classes.query import Filter
+
+from app.retrieval.abstract_retrieval import AbstractRetrieval
+from app.vector_database.lecture_schema import init_lecture_schema, LectureSchema
+
+
+class LectureRetrieval(AbstractRetrieval, ABC):
+    """
+    Class for retrieving lecture data from the database.
+    """
+
+    def __init__(self, client: WeaviateClient):
+        self.collection = init_lecture_schema(client)
+
+    def retrieve(
+        self,
+        user_message: str,
+        hybrid_factor: float,
+        result_limit: int,
+        lecture_id: int = None,
+        message_vector: [float] = None,
+    ) -> List[str]:
+        response = self.collection.query.hybrid(
+            query=user_message,
+            filters=(
+                Filter.by_property(LectureSchema.LECTURE_ID.value).equal(lecture_id)
+                if lecture_id
+                else None
+            ),
+            alpha=hybrid_factor,
+            vector=message_vector,
+            return_properties=[
+                LectureSchema.PAGE_TEXT_CONTENT.value,
+                LectureSchema.PAGE_IMAGE_DESCRIPTION.value,
+                LectureSchema.COURSE_NAME.value,
+            ],
+            limit=result_limit,
+        )
+        return response
---
+++ app/vector_database/database.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import weaviate
+from .lecture_schema import init_lecture_schema
+from .repository_schema import init_repository_schema
+import weaviate.classes as wvc
+
+logger = logging.getLogger(__name__)
+
+
+class VectorDatabase:
+    """
+    Class to interact with the Weaviate vector database
+    """
+
+    def __init__(self):
+        self.client = weaviate.connect_to_wcs(
+            cluster_url=os.getenv("WEAVIATE_CLUSTER_URL"),
+            auth_credentials=weaviate.auth.AuthApiKey(os.getenv("WEAVIATE_AUTH_KEY")),
+        )
+        self.repositories = init_repository_schema(self.client)
+        self.lectures = init_lecture_schema(self.client)
+
+    def __del__(self):
+        self.client.close()
+
+    def delete_collection(self, collection_name):
+        """
+        Delete a collection from the database
+        """
+        if self.client.collections.exists(collection_name):
+            if self.client.collections.delete(collection_name):
+                logger.info(f"Collection {collection_name} deleted")
+            else:
+                logger.error(f"Collection {collection_name} failed to delete")
+
+    def delete_object(self, collection_name, property_name, object_property):
+        """
+        Delete an object from the collection inside the databse
+        """
+        collection = self.client.collections.get(collection_name)
+        collection.data.delete_many(
+            where=wvc.query.Filter.by_property(property_name).equal(object_property)
+        )
