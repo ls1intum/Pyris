b1c6b46ebb14cadbcda82667ee29c8d9dd1860de
Patrick Bassner <patrick@bassner.de>
2024-05-16 23:02:23 +0200

    add logs

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import ORJSONResponse
 from fastapi import FastAPI
+from starlette.background import BackgroundTask
+from starlette.responses import Response
 
 from app.web.routers.health import router as health_router
 from app.web.routers.pipelines import router as pipelines_router
@@ -19,6 +21,24 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
     logging.error(f"{request}: {exc_str}")
     content = {'status_code': 10422, 'message': exc_str, 'data': None}
     return JSONResponse(content=content, status_code=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+def log_info(req_body, res_body):
+    logging.info(req_body)
+    logging.info(res_body)
+@app.middleware('http')
+async def some_middleware(request: Request, call_next):
+    req_body = await request.body()
+    response = await call_next(request)
+
+    res_body = b''
+    async for chunk in response.body_iterator:
+        res_body += chunk
+
+    task = BackgroundTask(log_info, req_body, res_body)
+    return Response(content=res_body, status_code=response.status_code,
+                    headers=dict(response.headers), media_type=response.media_type, background=task)
+
+
 
 app.include_router(health_router)
 app.include_router(pipelines_router)
