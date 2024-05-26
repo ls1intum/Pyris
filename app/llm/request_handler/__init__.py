6062f3921a7cc50fd26739d61852d6c0fa698d5d
Yassine Souissi <yassine.souissi@tum.de>
2024-05-26 03:11:54 +0200

    return the Basic request handler because needed for ingestion

--- app/llm/request_handler/__init__.py
+++ app/llm/request_handler/__init__.py
@@ -1,4 +1,6 @@
 from ..request_handler.request_handler_interface import RequestHandler
+from ..request_handler.basic_request_handler import BasicRequestHandler
+
 from ..request_handler.capability_request_handler import (
     CapabilityRequestHandler,
     CapabilityRequestHandlerSelectionMode,
