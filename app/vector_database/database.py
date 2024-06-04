ccbffbb8636c712171d2486a26039bf2d8fcc744
Yassine Souissi <yassine.souissi@tum.de>
2024-06-04 17:04:28 +0200

    Get weaviate production ready

--- app/vector_database/database.py
+++ app/vector_database/database.py
@@ -4,26 +4,9 @@ import weaviate
 from .lecture_schema import init_lecture_schema
 from .repository_schema import init_repository_schema
 from weaviate.classes.query import Filter
-
-import yaml
+from app.config import settings
 
 logger = logging.getLogger(__name__)
-
-
-def load_config(file_path):
-    """
-    Load the configuration file
-    """
-    with open(file_path, "r") as file:
-        config = yaml.safe_load(file)
-    return config
-
-
-weaviate_config = load_config(os.environ.get("APPLICATION_YML_PATH"))
-env_vars = weaviate_config.get("env_vars", {})
-host = env_vars.get("WEAVIATE_HOST")
-port: int = env_vars.get("WEAVIATE_PORT")
-grpc_port: int = env_vars.get("WEAVIATE_GRPC_PORT")
 
 
 class VectorDatabase:
@@ -33,7 +16,7 @@ class VectorDatabase:
 
     def __init__(self):
         self.client = weaviate.connect_to_local(
-            host=host, port=port, grpc_port=grpc_port
+            host=settings.weaviate.host, port=settings.weaviate.port, grpc_port=settings.weaviate.grpc_port
         )
         self.repositories = init_repository_schema(self.client)
         self.lectures = init_lecture_schema(self.client)
