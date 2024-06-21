590e687e8bdb6f77ea05c9633b0da31f821984a8
Yassine Souissi <yassine.souissi@tum.de>
2024-06-21 20:46:11 +0200

    Should be working fine now

--- app/pipeline/chat/output_models/output_models/selected_paragraphs.py
+++ app/pipeline/chat/output_models/output_models/selected_paragraphs.py
@@ -7,5 +7,5 @@ class SelectedParagraphs(BaseModel):
     selected_paragraphs: List[int] = Field(
         default=[],
         description="List of paragraphs sorted from most relevant to least relevant to the student question, "
-                    "each with a relevance score.",
+        "each with a relevance score.",
     )
