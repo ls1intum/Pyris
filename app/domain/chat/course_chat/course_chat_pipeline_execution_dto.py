c8b8cccdd9ae2b61097ba74dd955431becdd2b60
Kaan Çaylı <38523756+kaancayli@users.noreply.github.com>
2024-07-08 23:38:08 +0200

    Flake

--- app/domain/chat/course_chat/course_chat_pipeline_execution_dto.py
+++ app/domain/chat/course_chat/course_chat_pipeline_execution_dto.py
@@ -1,11 +1,9 @@
-from typing import Optional, Any, Union
+from typing import Optional, Any
 
 from pydantic import Field
 
 from ..chat_pipeline_execution_dto import ChatPipelineExecutionDTO
-from ...data.exercise_with_submissions_dto import ExerciseWithSubmissionsDTO
 from ...data.extended_course_dto import ExtendedCourseDTO
-from ...data.metrics.competency_jol_dto import CompetencyJolDTO
 from ...data.metrics.student_metrics_dto import StudentMetricsDTO
 from ...event.pyris_event_dto import PyrisEventDTO
 
