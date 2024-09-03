a0d35d2554dfb384edc54804d8ec7d985b98b03c
Patrick Bassner <patrick@bassner.de>
2024-09-03 12:03:23 +0200

    Added default values to SimpleSubmissionDTO fields

--- app/domain/data/simple_submission_dto.py
+++ app/domain/data/simple_submission_dto.py
@@ -6,5 +6,8 @@ from datetime import datetime
 
 
 class SimpleSubmissionDTO(BaseModel):
-    timestamp: Optional[datetime] = Field(alias="timestamp")
-    score: Optional[float] = Field(alias="score")
+    timestamp: Optional[datetime] = Field(alias="timestamp", default=None)
+    score: Optional[float] = Field(alias="score", default=0)
+
+    class Config:
+        require_by_default = False
