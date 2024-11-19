5e0b1025e381834edd9a4973331f4a3c23c14707
isabella <isabella.gessl@tum.de>
2024-11-19 12:18:42 +0100

    create weaviate migration

--- app/vector_database/lecture_schema.py
+++ app/vector_database/lecture_schema.py
@@ -31,15 +31,15 @@ def init_lecture_schema(client: WeaviateClient) -> Collection:
     """
     if client.collections.exists(LectureSchema.COLLECTION_NAME.value):
         collection = client.collections.get(LectureSchema.COLLECTION_NAME.value)
-
-        # collection.config.add_property(
-        #    Property(
-        #        name=LectureSchema.COURSE_LANGUAGE.value,
-        #        description="The language of the COURSE",
-        #        data_type=DataType.TEXT,
-        #        index_searchable=False,
-        #    )
-        # )
+        if not any((property.name == "course_language") for property in collection.config.get(simple=False).properties):
+            collection.config.add_property(
+               Property(
+                   name=LectureSchema.COURSE_LANGUAGE.value,
+                   description="The language of the COURSE",
+                   data_type=DataType.TEXT,
+                   index_searchable=False,
+               )
+            )
 
         return collection
 
