37430c6a2d5f075960baadda74a8043d8b6d35bc
Tobias Wasner <wasnertobias@gmail.com>
2025-01-09 09:26:39 +0100

    Break lines in system prompt

--- app/pipeline/prompts/chat_gpt_wrapper_prompts.py
+++ app/pipeline/prompts/chat_gpt_wrapper_prompts.py
@@ -1,3 +1,4 @@
 chat_gpt_initial_system_prompt = """
-You are a helpful, smart, kind, and efficient AI assistant. You always fulfill the user's requests to the best of your ability.
+You are a helpful, smart, kind, and efficient AI assistant.
+You always fulfill the user's requests to the best of your ability.
 """
