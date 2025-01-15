274f64079e8c4e139d0acf378923137bd2a364f8
Tobias Wasner <wasnertobias@gmail.com>
2025-01-15 18:06:15 +0100

    Make ChatGPTWrapper runnable once again

--- app/pipeline/chat_gpt_wrapper_pipeline.py
+++ app/pipeline/chat_gpt_wrapper_pipeline.py
@@ -8,13 +8,16 @@ from app.common.pyris_message import IrisMessageRole, PyrisMessage
 from app.domain.chat.exercise_chat.exercise_chat_pipeline_execution_dto import (
     ExerciseChatPipelineExecutionDTO,
 )
-from app.domain.data.text_message_content_dto import TextMessageContentDTO
+from langchain_core.output_parsers import StrOutputParser
+from app.llm.langchain.iris_langchain_chat_model import IrisLangchainChatModel
 from app.pipeline.prompts.chat_gpt_wrapper_prompts import chat_gpt_initial_system_prompt
 from langchain_core.messages import SystemMessage, HumanMessage
+from langchain_core.runnables import Runnable
 
 from app.llm import CapabilityRequestHandler, RequirementList, CompletionArguments
 from app.pipeline import Pipeline
 from app.web.status.status_update import ExerciseChatStatusCallback
+from ..common.message_converters import convert_iris_message_to_langchain_human_message
 
 logger = logging.getLogger(__name__)
 
@@ -47,17 +50,23 @@ def convert_chat_history_to_str(chat_history: List[PyrisMessage]) -> str:
 
 class ChatGPTWrapperPipeline(Pipeline):
     callback: ExerciseChatStatusCallback
-    request_handler: CapabilityRequestHandler
+    llm: IrisLangchainChatModel
+    pipeline: Runnable
 
     def __init__(self, callback: Optional[ExerciseChatStatusCallback] = None):
         super().__init__(implementation_id="chat_gpt_wrapper_pipeline_reference_impl")
         self.callback = callback
-        self.request_handler = CapabilityRequestHandler(
+        request_handler = CapabilityRequestHandler(
             requirements=RequirementList(
                 gpt_version_equivalent=4.5,
                 context_length=16385,
             )
         )
+        completion_args = CompletionArguments(temperature=0.5, max_tokens=2000)
+        self.llm = IrisLangchainChatModel(
+            request_handler=request_handler, completion_args=completion_args
+        )
+        self.pipeline = self.llm | StrOutputParser()
         self.tokens = []
 
     def __call__(
@@ -80,14 +89,14 @@ class ChatGPTWrapperPipeline(Pipeline):
 
         chat_history_messages = convert_chat_history_to_str(chat_history)
 
-        prompts = ChatPromptTemplate.from_messages(
+        prompt = ChatPromptTemplate.from_messages(
             [
                 SystemMessage(chat_gpt_initial_system_prompt),
                 HumanMessage(chat_history_messages),
+                convert_iris_message_to_langchain_human_message(query),
             ]
         )
 
-        response = self.request_handler.chat(
-            prompts, CompletionArguments(temperature=0.4)
-        )
+        response = (prompt | self.pipeline).invoke({})
         self.callback.done(final_result=response)
+        self.callback.skip("Skipping suggestion generation as no output was generated.")
