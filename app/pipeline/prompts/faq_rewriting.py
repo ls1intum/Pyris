0d9fb04e5f6e402dbe515687fd04a2278599d749
TIm Cremer <tim.cremer@tum.de>
2025-01-21 12:22:51 +0100

    fixed prompt once more

--- app/pipeline/prompts/faq_rewriting.py
+++ app/pipeline/prompts/faq_rewriting.py
@@ -11,13 +11,12 @@ convert them into complete sentences.
 6. Avoid repeating any information that is already present in the text.
 7. Make sure to keep the markdown formatting intact and add formatting for the most important information
 8. If someone does input a very short text, that does not resemble to be an answer to a potential question please make 
-sure to respond accordingly
+sure to respond accordingly. Also, if the input
 
 The text to be rephrased starts now:
-
+###START###
 {rewritten_text}
-
-The rephrased text ends prior to this line.
+###END###
 
 Respond with a single string containing only the improved version of the text. Your output will be used as an answer to 
 a frequently asked question (FAQ) on the Artemis platform, so make sure it is clear and concise.
