3daa452cd52aff4e07e2b9b9af1f18a37603ea55
Felix T.J. Dietrich <felix_dietrich@gmx.de>
2025-02-03 16:49:51 +0100

    Improved consistency check (#203)

--- app/pipeline/prompts/inconsistency_check_prompts.py
+++ app/pipeline/prompts/inconsistency_check_prompts.py
@@ -1,34 +1,63 @@
-basic_prompt = """\
+solver_prompt = """\
 <Instruction>
-As detail-oriented expert, find inconsistencies between the provided problem statement and the template repository of \
-a programming exercise.
-The student will use the the template repository to write code that solves the problem statement.
+You are a detail-oriented expert instructor at an Ivy League university ensuring the quality of programming exercises. \
+Your task is to find consistency issues as part of the exercise creation process to make sure that the exercise is \
+without any errors or inconsistencies that might confuse students. Your teaching assistants will use your feedback to \
+improve the exercise.
 
-Checks:
-- Given the problem statement, identify any missing or incorrect information in the template repository.
-- Given the template repository, identify any missing or incorrect information in the problem statement.
-- Ensure that the theme of the problem statement is consistent with the template repository.
-- Ensure that the problem statement is clear and concise and it covers everything that the student needs to know in \
-order to solve the exercise.
+Parts of a programming exercise:
+ - Problem statement: The description of the exercise containing tasks that the student needs to solve.
+ - Template repository: The starting point from which the student will start solving the exercise.
+ - Solution repository: The sample solution set by the instructor to compare the student's solution against.
 
-It is not an inconsistency, if the problem statement clearly states that the student is responsible for writing a \
-specific part of the code.
+To not overburden you, you will be provided with the problem statement and one of the template plus solution files \
+at a time. You need to compare the problem statement with the template file and identify any consistency issues.
 </Instruction>
 
-<Problem Statement>
+<ProblemStatement>
 {problem_statement}
-</Problem Statement>
+</ProblemStatement>
 
-<TemplateRepository>
-{template_repository}
-</TemplateRepository>
+<TemplateFile path='{file_path}'>
+{template_file}
+</TemplateFile>
+
+<SolutionFile path='{file_path}'>
+{solution_file}
+</SolutionFile>
 
 <Response>
-Be smart about it, give a structured and actionable response that an instructor can use to significantly improve the \
-exercise. Clearly state where the inconsistency lies. Do not make up inconsistencies just to have something to say.
-It needs to be very comprehensive and detailed, imagine some inconsistencies slipped through, students in the exam \
-will be confused and frustrated. This is a high stakes exam, so we need to be very thorough.
-You will be legally responsible for the quality of the exercise, so make sure you do the absolute best job possible, \
-otherwise you will be held accountable in the court of law. Do not quote whole files! 🔫
+Respond with any potential consistency issues found in the exercise formatted in markdown. \
+Just provide the easily digestible formatted markdown without other explanations. It is fine to provide no issues if \
+you are confident that the files are consistent.
+</Response>
+"""
+
+prettify_prompt = """\
+<Instruction>
+You are a detail-oriented expert instructor at an Ivy League university ensuring the quality of programming exercises. \
+Your task is to find consistency issues as part of the exercise creation process to make sure that the exercise is \
+without any errors or inconsistencies that might confuse students.
+In a previous step you already found potential consistency issues as part of the exercise creation process on a file \
+level. Now, you need to summarize the issues found in the exercise so the teaching assistants can fix them.
+
+Parts of a programming exercise:
+ - Problem statement: The description of the exercise containing tasks that the student needs to solve.
+ - Template repository: The starting point from which the student will start solving the exercise.
+ - Solution repository: The sample solution set by the instructor to compare the student's solution against.
+</Instruction>
+
+<ProblemStatement>
+{problem_statement}
+</ProblemStatement>
+
+<ConsistencyIssues>
+{consistency_issues}
+</ConsistencyIssues>
+
+<Response>
+Respond with a summary of the consistency issues found in the exercise, stay specific and clear so the issues can be \
+easily fixed by the teaching assistants. Make it clear which file path contains the issues. Just provide the easily \
+digestible formatted markdown without other explanations.
 </Response>
 """
